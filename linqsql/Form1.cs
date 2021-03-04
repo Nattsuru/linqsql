88f1456ff05de39a7e7d79e28e853fe5bf8a34f2
Alejandro <alejohervil@gmail.com>
2021-03-04 18:15:14 -0500

    Nuevo

--- linqsql/Form1.cs
+++ linqsql/Form1.cs
@@ -23,7 +23,7 @@ namespace linqsql
             data = new DataDataContext();
             var user = from u in data.GetTable<usuarios>()
                        select u;
-
+            //holi
             dgView.DataSource = user.ToList();
         }
     }
